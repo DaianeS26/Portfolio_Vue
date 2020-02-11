db280f6d10fd724d4f76af4a840892690cae9e95
Daianes26 <daianeasilva26@gmail.com>
2020-02-10 22:19:11 -0500

    Add vuex

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,18 +8,13 @@
   import Stock from './Stock.vue';
 
   export default {
-    data() {
-      return {
-        stocks: [
-          { id: 1, name: 'BMW', price: 110 },
-          { id: 2, name: 'Google', price: 130 },
-          { id: 3, name: 'Tesla', price: 150 },
-          { id: 4, name: 'Beyond Burger', price: 210 }
-        ]
-      }
-    },
     components: {
       appStock: Stock
+    },
+    computed: {
+      stocks() {
+        return this.$store.getters.stocks;
+      }
     }
   }
 </script>
